1740e4ab97fbb0c86f5ddd8a3f7000db298d6a00
Balaji <Balaji@LAPTOP-PP7LTPN0>
2019-09-01 20:47:18 +0530

    Moving Frameworks Project to GIT

---
+++ Frameworks/src/com/demoaut/newtours/TestScripts/VerifyInvalidCredentialsTest.java
@@ -0,0 +1,49 @@
+package com.demoaut.newtours.TestScripts;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.demoaut.newtours.Pages.Login;
+import com.demoaut.newtours.Pages.SignOn;
+
+import utils.GenericMethods;
+
+public class VerifyInvalidCredentialsTest {
+
+	WebDriver driver;
+	@BeforeTest
+	public void openApplication() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://newtours.demoaut.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void verifyInvalidLoginTest() throws IOException {
+		Login lp = new Login(driver);
+		SignOn so = new SignOn(driver);
+
+		String[][] data = GenericMethods.getData("D:\\Sel4Aug\\TestData.xlsx", "Sheet1");
+		for(int i=1;i<data.length;i++) {
+			lp.applicationLogin(data[i][0], data[i][1]);
+			boolean signOnVisible = so.verifySignOnLinkVisible();
+			Assert.assertTrue(signOnVisible);
+			driver.navigate().back();
+		}
+	}
+
+	@AfterTest
+	public void closeApplication() {
+		driver.close();
+	}
+
+}
---
+++ Frameworks/src/kdf/Application.java
@@ -0,0 +1,49 @@
+package kdf;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import utils.GenericMethods;
+
+public class Application {
+	
+	@Test
+	public void verifyInvalidLogin() throws IOException {
+		String[][] data = GenericMethods.getData("D:\\Sel4Aug\\TestData.xlsx", "Sheet2");
+		for(int i =1;i<data.length;i++) {
+			switch(data[i][3]) {
+			case "openBrowser":
+				Methods.openBrowser();
+				break;
+			case "maxBrowser":
+				Methods.maximizeBrowser();
+				break;
+			case "impWait":
+				Methods.implicitWait();
+				break;
+			case "navigateToApplication":
+				Methods.navigateToApplication(data[i][6]);
+				break;
+			case "enterUserID":
+				Methods.enterUserID(data[i][4],data[i][5], data[i][6]);
+				break;
+			case "enterPassword":
+				Methods.enterPassword(data[i][4],data[i][5], data[i][6]);
+				break;
+			case "clickSignIn":
+				Methods.clickSignIn(data[i][4],data[i][5]);
+				break;
+			case "verifyErrMsg":
+				String actualMsg = Methods.verifyErrorMessage(data[i][5]);
+				Assert.assertEquals(actualMsg, data[i][6]);
+				break;
+			case "closeAUT":
+				Methods.closeApplication();
+				break;
+			}
+		}
+	}
+
+}
